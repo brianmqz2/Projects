e6d45ebb6ad02024a89648c0a17a5b4ca3046a3f
brianmqz2 <114853457+brianmqz2@users.noreply.github.com>
2022-10-16 20:07:07 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,66 +1,42 @@
 ﻿using System;
 using static System.Console;
 
-namespace randomNum_Project
+namespace ProgrammingAssignment2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            /*This program generates random numbers and 
-             the position for the maximum/minimum numbers*/
-            int[] intArray = new int[50];
-            Random randomNum = new Random();
-            int randomNumber;
+            /*This program evaluates the are of a triangle
+            and determines if a triangle can be formed or not*/
+            double triangleArea;
+            double a, b, c, s;
 
-            /*The following for() loop outputs the 50 random 
-             numbers in a 5 row by 10 column format*/
-            for (int i = 0; i < intArray.Length; i++)
+            // asking for the length of 3 lines of a triangle
+            Write("Enter first line length: ");
+            a = Convert.ToInt32(ReadLine());
+            Write("Enter second line length: ");
+            b = Convert.ToInt32(ReadLine());
+            Write("Enter third line length: ");
+            c = Convert.ToInt32(ReadLine());
+            //multiply a, b, c then devide by 2 to get semi-perimeter
+            s = (a + b + c) / 2;
+            //calculating formula shown to find area of a triangle
+            triangleArea = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+
+            if (a + b > c & b + c > a & c + a > b)
+
             {
+                //prints answer correctily to the nearest hundreth decimal
+                WriteLine("\n The area of the triangle is: {0:N2}", triangleArea);
 
-                randomNumber = randomNum.Next(1, 100);//This code restricts the code from 1 <= x <= 99
-                intArray[i] = randomNumber;
-                {
-                    /*This if() statement allows the 50 numbers 
-                     to be outputted in a set of 10 columns*/
-                    if (i % 10 == 0)
-                        WriteLine("");
-                    Write("{0, 4}", randomNumber);
-                }
             }
 
-            int maxNum = intArray[0], minNum = intArray[0];
-            int posNum1 = 0, posNum2 = 0;
-
-            /*This for() loop outputs the WriteLine part 
-             of the program which gives us the
-            maximum/minimum numbers and their position*/
-            for (int i = 0; i < intArray.Length; i++)
-            {
-                //This gives us the maximum and its position
-                if (intArray[i] > maxNum)
-                {
-                    maxNum = intArray[i];
-                    posNum1 = i;
-                }
-                //This gives us the minimum and its position
-                if (intArray[i] < minNum)
-                {
-                    minNum = intArray[i];
-                    posNum2 = i;
-                }
-            }
-
-            /*The following is what get outputted in the 
-             terminal which is based of the randomly
-            generated numbers and its position*/
-
-            WriteLine("\n");
-            WriteLine("The highest number is {0} and it is at position {1}", maxNum, posNum1);
-            WriteLine("The lowest number is {0} and it is at position {1}", minNum, posNum2);
+            else
+                //print if rules aren't followed in previous if() statement
+                WriteLine("\n Lines cannot form a triangle.");
 
         }
     }
-
 }
 
